d190e2ad5d5b7fd127d4d4b9e67b6a4b5acc661a
Yousef Said <jooasaid2004@gmail.com>
2026-04-18 13:34:59 +0200

    Add project files.

---
+++ LINQ02/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace LINQ02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region Q01
+
+            #endregion
+
+
+        }
+    }
+}
